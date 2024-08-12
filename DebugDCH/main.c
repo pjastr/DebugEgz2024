0181cfd5c35cdc2afea2d9bd9b1818e9582fa0bb
pjastr <24464854+pjastr@users.noreply.github.com>
2024-08-12 13:43:37 +0200

    add projects

---
+++ DebugDCH/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char napis[20]="2024#programowanie";
+    char z = *(napis+3); //z=
+    int a = napis[5]; // a=   ,z=
+    z++; // a=   ,z=
+    int b= a++; // a=   , b=  ,z=
+    a=a-4; // a=   , b=  ,z=
+    z-=5; // a=   , b=  ,z=
+    z=(a-=2)+3; // a=   , b=  ,z=
+    return 0;
+}
